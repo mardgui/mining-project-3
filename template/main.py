d69b4b557c58042724766a98c54474d83cd70eab
Quentin Guimard <quentin.guimard@etu.unice.fr>
2018-12-20 22:03:13 +0100

    task 2 try

--- template/main.py
+++ template/main.py
@@ -10,7 +10,7 @@ import sys
 import numpy
 from gspan_mining import GraphDatabase
 from gspan_mining import gSpan
-from sklearn import metrics
+from sklearn import metrics, tree
 from sklearn import naive_bayes
 
 
@@ -255,6 +255,59 @@ def example2():
             train_and_evaluate(minsup, graph_database, subsets)
 
 
+def task2():
+    args = sys.argv
+    database_file_name_pos = args[1]  # First parameter: path to positive class file
+    database_file_name_neg = args[2]  # Second parameter: path to negative class file
+    k = int(args[3])  # Third parameter: k
+    minsup = int(args[4])  # Fourth parameter: minimum support
+    nfolds = int(args[5])  # Fifth parameter: number of folds to use in the k-fold cross-validation.
+
+    if not os.path.exists(database_file_name_pos):
+        print('{} does not exist.'.format(database_file_name_pos))
+        sys.exit()
+    if not os.path.exists(database_file_name_neg):
+        print('{} does not exist.'.format(database_file_name_neg))
+        sys.exit()
+
+    graph_database = GraphDatabase()  # Graph database object
+    pos_ids = graph_database.read_graphs(
+        database_file_name_pos)  # Reading positive graphs, adding them to database and getting ids
+    neg_ids = graph_database.read_graphs(
+        database_file_name_neg)  # Reading negative graphs, adding them to database and getting ids
+
+    # If less than two folds: using the same set as training and test set (note this is not an accurate way to evaluate the performances!)
+    if nfolds < 2:
+        subsets = [
+            pos_ids,  # Positive training set
+            pos_ids,  # Positive test set
+            neg_ids,  # Negative training set
+            neg_ids  # Negative test set
+        ]
+        # Printing fold number:
+        print('fold {}'.format(1))
+        train_and_evaluate(minsup, graph_database, subsets)
+
+    # Otherwise: performs k-fold cross-validation:
+    else:
+        pos_fold_size = len(pos_ids) // nfolds
+        neg_fold_size = len(neg_ids) // nfolds
+        for i in range(nfolds):
+            # Use fold as test set, the others as training set for each class;
+            # identify all the subsets to be maintained by the graph mining algorithm.
+            subsets = [
+                numpy.concatenate((pos_ids[:i * pos_fold_size], pos_ids[(i + 1) * pos_fold_size:])),
+                # Positive training set
+                pos_ids[i * pos_fold_size:(i + 1) * pos_fold_size],  # Positive test set
+                numpy.concatenate((neg_ids[:i * neg_fold_size], neg_ids[(i + 1) * neg_fold_size:])),
+                # Negative training set
+                neg_ids[i * neg_fold_size:(i + 1) * neg_fold_size],  # Negative test set
+            ]
+            # Printing fold number:
+            print('fold {}'.format(i + 1))
+            train_and_evaluate_2(minsup, graph_database, subsets, k)
+
+
 def train_and_evaluate(minsup, database, subsets):
     task = FrequentPositiveGraphs(minsup, database, subsets)  # Creating task
 
@@ -286,6 +339,37 @@ def train_and_evaluate(minsup, database, subsets):
     print()  # Blank line to indicate end of fold.
 
 
+def train_and_evaluate_2(minsup, database, subsets, k):
+    task = TopKConfident(minsup, database, subsets, k)  # Creating task
+
+    gSpan(task).run()  # Running gSpan
+
+    # Creating feature matrices for training and testing:
+    features = task.get_feature_matrices()
+    train_fm = numpy.concatenate((features[0], features[2]))  # Training feature matrix
+    train_labels = numpy.concatenate(
+        (numpy.full(len(features[0]), 1, dtype=int), numpy.full(len(features[2]), -1, dtype=int)))  # Training labels
+    test_fm = numpy.concatenate((features[1], features[3]))  # Testing feature matrix
+    test_labels = numpy.concatenate(
+        (numpy.full(len(features[1]), 1, dtype=int), numpy.full(len(features[3]), -1, dtype=int)))  # Testing labels
+
+    classifier = tree.DecisionTreeClassifier(random_state=1)  # Creating model object
+    classifier.fit(train_fm, train_labels)  # Training model
+
+    predicted = classifier.predict(test_fm)  # Using model to predict labels of testing data
+
+    accuracy = metrics.accuracy_score(test_labels, predicted)  # Computing accuracy:
+
+    # Printing frequent patterns along with their positive support:
+    for t in task.top:
+        for pattern in t[2]:
+            print('{} {} {}'.format(pattern, t[0], t[1]))
+    # printing classification results:
+    print(predicted.tolist())
+    print('accuracy: {}'.format(accuracy))
+    print()  # Blank line to indicate end of fold.
+
+
 if __name__ == '__main__':
-    task1()
-    # example2()
+    # task1()
+    task2()

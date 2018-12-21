ec7073cf50fbe5b6be2f996a0cdd51669e4b6870
Quentin Guimard <quentin.guimard@etu.unice.fr>
2018-12-21 10:11:27 +0100

    Clean

--- template/main.py
+++ template/main.py
@@ -11,7 +11,6 @@ import numpy
 from gspan_mining import GraphDatabase
 from gspan_mining import gSpan
 from sklearn import metrics, tree
-from sklearn import naive_bayes
 
 
 class PatternGraphs:
@@ -97,7 +96,34 @@ class FrequentPositiveGraphs(PatternGraphs):
         return [numpy.array(matrix).transpose() for matrix in matrices]
 
 
-class TopKConfident(FrequentPositiveGraphs):
+class TopKConfident2(FrequentPositiveGraphs):
+    def __init__(self, minsup, database, subsets, k):
+        super().__init__(minsup, database, subsets)
+        self.top = []
+        self.k = k
+
+    def prune(self, gid_subsets):
+        # first subset is the set of positive ids
+        return len(gid_subsets[0] + gid_subsets[1]) < self.minsup
+
+    def store(self, dfs_code, gid_subsets):
+        total_support = len(gid_subsets[0]) + len(gid_subsets[1])
+        confidence = len(gid_subsets[0]) / total_support
+        if len(self.top) < self.k or confidence >= self.top[self.k - 1][0]:
+            found = False
+            for i, t in enumerate(self.top):
+                if t[0] == confidence and t[1] == total_support:
+                    t[2].append(dfs_code)
+                    found = True
+            if not found:
+                self.top.append((confidence, total_support, [dfs_code]))
+                self.top = sorted(self.top, reverse=True, key=lambda x: (x[0], x[1]))
+                if len(self.top) > self.k:
+                    del self.top[-1]
+        self.patterns.append((dfs_code, gid_subsets))
+
+
+class TopKConfident4(FrequentPositiveGraphs):
     def __init__(self, minsup, database, subsets, k):
         super().__init__(minsup, database, subsets)
         self.top = []
@@ -122,42 +148,6 @@ class TopKConfident(FrequentPositiveGraphs):
                 if len(self.top) > self.k:
                     del self.top[-1]
         self.patterns.append((dfs_code, gid_subsets))
-
-
-def example1():
-    """
-    Runs gSpan with the specified positive and negative graphs, finds all frequent subgraphs in the positive class
-    with a minimum positive support of minsup and prints them.
-    """
-
-    args = sys.argv
-    database_file_name_pos = args[1]  # First parameter: path to positive class file
-    database_file_name_neg = args[2]  # Second parameter: path to negative class file
-    minsup = int(args[3])  # Third parameter: minimum support
-
-    if not os.path.exists(database_file_name_pos):
-        print('{} does not exist.'.format(database_file_name_pos))
-        sys.exit()
-    if not os.path.exists(database_file_name_neg):
-        print('{} does not exist.'.format(database_file_name_neg))
-        sys.exit()
-
-    graph_database = GraphDatabase()  # Graph database object
-    pos_ids = graph_database.read_graphs(
-        database_file_name_pos)  # Reading positive graphs, adding them to database and getting ids
-    neg_ids = graph_database.read_graphs(
-        database_file_name_neg)  # Reading negative graphs, adding them to database and getting ids
-
-    subsets = [pos_ids, neg_ids]  # The ids for the positive and negative labelled graphs in the database
-    task = FrequentPositiveGraphs(minsup, graph_database, subsets)  # Creating task
-
-    gSpan(task).run()  # Running gSpan
-
-    # Printing frequent patterns along with their positive support:
-    for pattern, gid_subsets in task.patterns:
-        pos_support = len(gid_subsets[0])  # This will have to be replaced by the confidence and support on both classes
-        neg_support = len(gid_subsets[1])
-        print('{} {}'.format(pattern, pos_support + neg_support))
 
 
 def task1():
@@ -186,73 +176,13 @@ def task1():
         database_file_name_neg)  # Reading negative graphs, adding them to database and getting ids
 
     subsets = [pos_ids, neg_ids]  # The ids for the positive and negative labelled graphs in the database
-    task = TopKConfident(minsup, graph_database, subsets, k)  # Creating task
+    task = TopKConfident2(minsup, graph_database, subsets, k)  # Creating task
 
     gSpan(task).run()  # Running gSpan
 
     for t in task.top:
         for pattern in t[2]:
             print('{} {} {}'.format(pattern, t[0], t[1]))
-
-
-def example2():
-    """
-    Runs gSpan with the specified positive and negative graphs; finds all frequent subgraphs in the training subset of
-    the positive class with a minimum support of minsup.
-    Uses the patterns found to train a naive bayesian classifier using Scikit-learn and evaluates its performances on
-    the test set.
-    Performs a k-fold cross-validation.
-    """
-
-    args = sys.argv
-    database_file_name_pos = args[1]  # First parameter: path to positive class file
-    database_file_name_neg = args[2]  # Second parameter: path to negative class file
-    minsup = int(args[3])  # Third parameter: minimum support (note: this parameter will be k in case of top-k mining)
-    nfolds = int(args[4])  # Fourth parameter: number of folds to use in the k-fold cross-validation.
-
-    if not os.path.exists(database_file_name_pos):
-        print('{} does not exist.'.format(database_file_name_pos))
-        sys.exit()
-    if not os.path.exists(database_file_name_neg):
-        print('{} does not exist.'.format(database_file_name_neg))
-        sys.exit()
-
-    graph_database = GraphDatabase()  # Graph database object
-    pos_ids = graph_database.read_graphs(
-        database_file_name_pos)  # Reading positive graphs, adding them to database and getting ids
-    neg_ids = graph_database.read_graphs(
-        database_file_name_neg)  # Reading negative graphs, adding them to database and getting ids
-
-    # If less than two folds: using the same set as training and test set (note this is not an accurate way to evaluate the performances!)
-    if nfolds < 2:
-        subsets = [
-            pos_ids,  # Positive training set
-            pos_ids,  # Positive test set
-            neg_ids,  # Negative training set
-            neg_ids  # Negative test set
-        ]
-        # Printing fold number:
-        print('fold {}'.format(1))
-        train_and_evaluate(minsup, graph_database, subsets)
-
-    # Otherwise: performs k-fold cross-validation:
-    else:
-        pos_fold_size = len(pos_ids) // nfolds
-        neg_fold_size = len(neg_ids) // nfolds
-        for i in range(nfolds):
-            # Use fold as test set, the others as training set for each class;
-            # identify all the subsets to be maintained by the graph mining algorithm.
-            subsets = [
-                numpy.concatenate((pos_ids[:i * pos_fold_size], pos_ids[(i + 1) * pos_fold_size:])),
-                # Positive training set
-                pos_ids[i * pos_fold_size:(i + 1) * pos_fold_size],  # Positive test set
-                numpy.concatenate((neg_ids[:i * neg_fold_size], neg_ids[(i + 1) * neg_fold_size:])),
-                # Negative training set
-                neg_ids[i * neg_fold_size:(i + 1) * neg_fold_size],  # Negative test set
-            ]
-            # Printing fold number:
-            print('fold {}'.format(i + 1))
-            train_and_evaluate(minsup, graph_database, subsets)
 
 
 def task2():
@@ -308,39 +238,8 @@ def task2():
             train_and_evaluate_2(minsup, graph_database, subsets, k)
 
 
-def train_and_evaluate(minsup, database, subsets):
-    task = FrequentPositiveGraphs(minsup, database, subsets)  # Creating task
-
-    gSpan(task).run()  # Running gSpan
-
-    # Creating feature matrices for training and testing:
-    features = task.get_feature_matrices()
-    train_fm = numpy.concatenate((features[0], features[2]))  # Training feature matrix
-    train_labels = numpy.concatenate(
-        (numpy.full(len(features[0]), 1, dtype=int), numpy.full(len(features[2]), -1, dtype=int)))  # Training labels
-    test_fm = numpy.concatenate((features[1], features[3]))  # Testing feature matrix
-    test_labels = numpy.concatenate(
-        (numpy.full(len(features[1]), 1, dtype=int), numpy.full(len(features[3]), -1, dtype=int)))  # Testing labels
-
-    classifier = naive_bayes.GaussianNB()  # Creating model object
-    classifier.fit(train_fm, train_labels)  # Training model
-
-    predicted = classifier.predict(test_fm)  # Using model to predict labels of testing data
-
-    accuracy = metrics.accuracy_score(test_labels, predicted)  # Computing accuracy:
-
-    # Printing frequent patterns along with their positive support:
-    for pattern, gid_subsets in task.patterns:
-        pos_support = len(gid_subsets[0])
-        print('{} {}'.format(pattern, pos_support))
-    # printing classification results:
-    print(predicted.tolist())
-    print('accuracy: {}'.format(accuracy))
-    print()  # Blank line to indicate end of fold.
-
-
 def train_and_evaluate_2(minsup, database, subsets, k):
-    task = TopKConfident(minsup, database, subsets, k)  # Creating task
+    task = TopKConfident4(minsup, database, subsets, k)  # Creating task
 
     gSpan(task).run()  # Running gSpan
 
